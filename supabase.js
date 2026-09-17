10520d9bb388b9535f5858202f8fd17795ce8d51
EdgardSk8 <osais1000@gmail.com>
2026-09-16 22:38:56 -0400

    primer commit

---
+++ supabase.js
@@ -0,0 +1,9 @@
+// const SUPABASE_URL = 'https://wumnnmhkbcnzuexlqfne.supabase.co';
+// const SUPABASE_ANON_KEY = 'sb_publishable_6coHdJkAuJ-92vG_gYTctA_mo8z4eaE';
+// const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+
+
+const SUPABASE_URL = 'https://vkblifegvwjnfmxmirrc.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_CXVL1qE1_KK4aAsDiI6VuQ_V2Sf8NU_';
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
